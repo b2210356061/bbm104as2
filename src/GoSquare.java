cfe3f605dc3f9de1b43cc1b8f7fe047b12ceb339
b2210356061 <b2210356061@cs.hacettepe.edu.tr>
2022-04-01 17:11:09 +0300

    Serious bugfix

--- src/GoSquare.java
+++ src/GoSquare.java
@@ -1,8 +1,6 @@
 public class GoSquare extends Square {
     @Override
     public String takeAction(Player player, int dice) throws BankruptException {
-        player.addBalance(200);
-        Monopoly.banker.removeBalance(200);
         return player.getName() + " is in GO square";
     }
 }
