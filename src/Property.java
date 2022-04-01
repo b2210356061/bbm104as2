e2b111277d7fd8e496016075920d8cd3a8b7c139
b2210356061 <b2210356061@cs.hacettepe.edu.tr>
2022-04-01 16:55:17 +0300

    Player goes bankrupt when the balance is 0

--- src/Property.java
+++ src/Property.java
@@ -44,7 +44,7 @@ public class Property extends Square {
     @Override
     public String takeAction(Player visitor, int dice) throws BankruptException {
         if (owner == null) { // Nobody owns this property
-            if (visitor.getBalance() >= cost) { // player will buy if he/she can afford it
+            if (visitor.getBalance() > cost) { // player will buy if he/she can afford it
                 owner = visitor;
                 owner.addProperty(this.name);
                 visitor.removeBalance(cost);
